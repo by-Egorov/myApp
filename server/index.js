caa111f03d39bca2a7e398ae2a74c53d0ff37e8e
EgorovAlexandr <egorov.dev@gmail.com>
2023-12-07 06:54:33 +0300

    the server is running

--- server/index.js
+++ server/index.js
@@ -2,9 +2,9 @@ import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
-import authRouter from './routers/authRouter.js'
+import authRouter from './routes/authRouter.js'
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5555
 const db = process.env.DB_NAME
 const user = process.env.DB_USER_NAME
 const password = process.env.DB_PASSWORD
