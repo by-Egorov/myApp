edfa3a9e07dfefff2c09a6a4239247b322168203
EgorovAlexandr <egorov.dev@gmail.com>
2023-12-07 06:38:04 +0300

    Starting the server and connecting to the database

---
+++ server/index.js
@@ -0,0 +1,27 @@
+import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+
+const PORT = process.env.PORT || 5000
+const db = process.env.DB_NAME
+const user = process.env.DB_USER_NAME
+const password = process.env.DB_PASSWORD
+const app = express()
+
+mongoose
+	.connect(
+		`mongodb+srv://${user}:${password}@cluster0.5qqffmc.mongodb.net/${db}?retryWrites=true&w=majority`
+	)
+	.then(() => console.log('DB ok'))
+	.catch(error => console.log('DB error', error))
+
+app.use(express.json())
+app.use(cors())
+
+app.listen(PORT, error => {
+	if (error) {
+		return console.warn(error)
+	}
+	console.log(`Сервер запущен на порту ${PORT}`)
+})
