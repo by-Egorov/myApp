1172410da300e4f31b063e30da92fa2f00f7d798
Александр <109337223+by-Egorov@users.noreply.github.com>
2023-12-14 06:58:51 +0300

    Update axios.js baseUrl

--- myApp/src/axios.js
+++ myApp/src/axios.js
@@ -1,12 +1,12 @@
 import axios from 'axios'
 
 const $host = axios.create({
-    baseURL: 'http://localhost:8080/api',
-    // baseURL: 'https://mycar-x95z.onrender.com/api',
+    //baseURL: 'http://localhost:8080/api',
+    baseURL: 'https://mycar-x95z.onrender.com/api',
 })
 const $authHost = axios.create({
-    baseURL: 'http://localhost:8080/api',
-    // baseURL: 'https://mycar-x95z.onrender.com/api',
+    //baseURL: 'http://localhost:8080/api',
+    baseURL: 'https://mycar-x95z.onrender.com/api',
 })
 const authInterceptor = (config) => {
     config.headers.authorization = `Bearer ${JSON.parse(localStorage.getItem('token'))}`
