dbdb3fbb2225254a86ab3253af90b4cf854a8d87
EgorovAlexandr <egorov.dev@gmail.com>
2024-04-20 06:28:30 +0300

    fix

--- myApp/src/axios.js
+++ myApp/src/axios.js
@@ -15,21 +15,3 @@ const authInterceptor = (config) => {
 $authHost.interceptors.request.use(authInterceptor)
 
 export { $host, $authHost }
-
-import axios from 'axios'
-
-const $host = axios.create({
-    // baseURL: 'http://localhost:8080/api',
-    baseURL: 'https://mycar-x95z.onrender.com/api',
-})
-const $authHost = axios.create({
-    // baseURL: 'http://localhost:8080/api',
-    baseURL: 'https://mycar-x95z.onrender.com/api',
-})
-const authInterceptor = (config) => {
-    config.headers.authorization = `Bearer ${JSON.parse(localStorage.getItem('token'))}`
-    return config
-}
-$authHost.interceptors.request.use(authInterceptor)
-
-export { $host, $authHost }
